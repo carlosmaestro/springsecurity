38968dacb405991db5a7ae551e94b5ca60a103ed
Carlos Maestro <chmmaestro@gmail.com>
2025-10-19 13:36:04 -0300

    first commit

---
+++ src/main/java/com/chmmaestro/springsecurity/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.chmmaestro.springsecurity.repository;
+
+
+import com.chmmaestro.springsecurity.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
